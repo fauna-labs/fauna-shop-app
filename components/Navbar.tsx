91f5651b8bded9576684eb4fb660f1ed80664223
Shadid12 <Shadid12@users.noreply.github.com>
2023-03-05 17:07:11 -0500

    updated filter styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import marketIcon from "../public/market.svg";
+import filterIcon from "../public/filter.svg";
 import styles from "../styles/Nav.module.css";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="container mx-auto">
-      <nav className="p-3 flex justify-between max-sm:flex-col">
+      <nav className={styles.navStyle}>
         <div className="flex w-3/4 max-sm:w-full">
           <Link className={styles.logo} href="/">
             <Image
@@ -25,7 +26,16 @@ export default function Navbar() {
           />
           <button className={styles.searchBtn}>🔍</button>
         </div>
-        <div className="flex max-sm:mt-1">
+        <div className={styles.btnWraps}>
+          <button className="border border-gray-800 rounded-md p-2 mr-3">
+            <Image
+              priority
+              src={filterIcon}
+              height={20}
+              width={20}
+              alt="market Icon"
+            />
+          </button>
           <button className="border border-gray-800 rounded-md p-2">Login</button>
         </div>
       </nav>
