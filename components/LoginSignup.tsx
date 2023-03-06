eaee88b540a2bd48714da1ca532ae72135daec69
Shadid12 <Shadid12@users.noreply.github.com>
2023-03-06 13:26:00 -0500

    done making the login and signup screens

--- components/LoginSignup.tsx
+++ components/LoginSignup.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import styles from "../styles/Nav.module.css";
 import Image from 'next/image';
 import crossIcon from "../public/cross.svg";
 
+const inputStyles = `
+p-2 mb-2 mt-2 rounded-lg border border-gray-800 w-full focus:outline-none focus:ring focus:ring-violet-300
+`
+const btnStyles = `mb-1 mt-2 border border-gray-800 rounded-md p-2 hover:bg-gray-200`
+
 export default function LoginSignup({ onClose } : { onClose: () => void }) {
+  const [showSignup, setShowSignup] = useState(false);
+
+  const doSignup = (e: any) => {
+    e.preventDefault();
+    console.log(e.target.username.value);
+  }
+
   return (
     <div className={styles.mainModal}>
       <div className={styles.modal}>
@@ -16,6 +29,55 @@ export default function LoginSignup({ onClose } : { onClose: () => void }) {
             />
           </button>
         </div>
+
+        { showSignup ? (
+          <div className="flex flex-col p-3">
+            <h2 className="text-xl pb-2 pt-2">Sign Up</h2>
+            <form onSubmit={doSignup} className="flex flex-col">
+              <div>
+                <label htmlFor="username">Username</label>
+                <input name="username" placeholder="Username" className={inputStyles}/>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email">Email</label>
+                <input placeholder="jon@email.com" name="email" className={inputStyles}/>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email">Password</label>
+                <input type="password" name="password" placeholder="Password" className={inputStyles}/>
+              </div>
+              <button type="submit" className={btnStyles}>Sign Up</button>
+            </form>
+            <button 
+              className="mt-3 underline hover:decoration-purple-800"
+              onClick={() => setShowSignup(false)}
+            >
+              Already have an account Login now.
+            </button>
+          </div>
+        ) : (
+          <div className="flex flex-col p-3">
+            <h2 className="text-xl pb-2 pt-2">Login</h2>
+            <form onSubmit={doSignup} className="flex flex-col">
+              <div className="flex flex-col">
+                <label htmlFor="email">Email</label>
+                <input placeholder="jon@email.com" name="email" className={inputStyles}/>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email">Password</label>
+                <input type="password" name="password" placeholder="Password" className={inputStyles}/>
+              </div>
+              <button type="submit" className={btnStyles}>Login</button>
+            </form>
+            <button 
+              className="mt-3 underline hover:decoration-purple-800"
+              onClick={() => setShowSignup(true)}
+            >
+              Don't have an account Signup now.
+            </button>
+          </div>
+        ) }
+
       </div>
     </div>
   )
