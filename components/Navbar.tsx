546957756da529ee9233ed2b2de8f3c950db110b
Shadid12 <Shadid12@users.noreply.github.com>
2023-03-05 22:42:00 -0500

    ready to add multi select

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import marketIcon from "../public/market.svg";
 import filterIcon from "../public/filter.svg";
 import styles from "../styles/Nav.module.css";
 import Link from "next/link";
 
+
 export default function Navbar() {
+  const [showFilter, setShowFilter] = useState(false);
+  
+
   return (
     <div className="container mx-auto">
       <nav className={styles.navStyle}>
@@ -27,7 +32,10 @@ export default function Navbar() {
           <button className={styles.searchBtn}>🔍</button>
         </div>
         <div className={styles.btnWraps}>
-          <button className="border border-gray-800 rounded-md p-2 mr-3">
+          <button 
+            className="border border-gray-800 rounded-md p-2 mr-3 hover:bg-gray-200"
+            onClick={() => setShowFilter(!showFilter)}
+          >
             <Image
               priority
               src={filterIcon}
@@ -36,9 +44,31 @@ export default function Navbar() {
               alt="market Icon"
             />
           </button>
-          <button className="border border-gray-800 rounded-md p-2">Login</button>
+          <button className="border border-gray-800 rounded-md p-2 hover:bg-gray-200">Login</button>
         </div>
       </nav>
+
+      {/* Search Filters */}
+      {showFilter && (
+        <div className={styles.filterContainer}>
+          <div>
+            <h3>Price $</h3>
+            <input 
+              type="number" 
+              placeholder="Min $" 
+              className="p-1 border border-gray-800 rounded-md mb-1 mr-1"
+            />
+            <input 
+              type="number" 
+              placeholder="Max $" 
+              className="p-1 border border-gray-800 rounded-md mb-1"
+            />
+          </div>
+          <button className="border border-gray-800 rounded-md p-2 hover:bg-gray-200" >Apply</button>
+        </div>
+      )}
+
+
     </div>
   )
 }
