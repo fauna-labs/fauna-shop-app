1fb6b0c6d5057d21546e962e15cd3f0947458dba
Shadid12 <Shadid12@users.noreply.github.com>
2023-03-07 01:41:57 -0500

    Update basic shop creation

--- pages/myshops.tsx
+++ pages/myshops.tsx
@@ -1,5 +1,6 @@
 import { Client } from '@fauna/fauna';
-import { useEffect } from 'react';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { useUserContext } from "../store/state";
 
 
@@ -7,11 +8,11 @@ export default function MyShopsPage() {
   const userCtx = useUserContext() as any;
   const { user } = userCtx;
 
-  console.log('user ==> MyShops', user);
+  const [shops, setShops] = useState([]);
   
   const client = new Client({ 
     endpoint: process.env.NEXT_PUBLIC_FAUNA_ENDPOINT as any,
-    secret: user ? user.data.secret : '' 
+    secret: user.data ? user.data.secret : process.env.NEXT_PUBLIC_FAUNA_SECRET as any, 
   });
 
   useEffect(() => {
@@ -21,9 +22,13 @@ export default function MyShopsPage() {
   const fetchShops = async () => {
     try {
       const response = await client.query({
-        query: `Shop.all.where("${user?.data.email}")`
+        query: `
+          let user = User.byId("358513799094861901")
+          Shop.all.where(.owner == user)
+        `
       })
-      console.log('response', response);
+      console.log('response: ==>', response);
+      setShops(response.data?.data);
     } catch (error) {
       console.log('error', error);
     }
@@ -41,9 +46,17 @@ export default function MyShopsPage() {
     <div className="container mx-auto">
       <h1 className="p-2 text-xl">Mange Your Shop(s)</h1>
       <div className="p-2 grid grid-cols-4 gap-4 max-sm:grid-cols-2">
-        <div className="border border-gray-900 p-3 rounded-md max-w-xs">01</div>
-        <div className="border border-gray-900 p-3 rounded-md max-w-xs">01</div>
-        <div className="border border-gray-900 p-3 rounded-md max-w-xs">01</div>
+        {
+          shops.map((shop: any) => (
+            <Link 
+              key={shop.id}
+              href={`/shops/${shop.id}`}
+              className="border border-gray-900 p-3 rounded-md max-w-xs cursor-pointer hover:bg-gray-200">
+              <h1 className="text-xl">{shop.name}</h1>
+              <p className="text-sm">{shop.description}</p>
+            </Link>
+          ))
+        }
       </div>
     </div>
   )
